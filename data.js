e2b44beadd7b58308ff279438d35db575d55103b
ItsukaHiro <itsukahiro.personal@proton.me>
2026-06-03 19:24:11 +0700

    first upload

---
+++ data.js
@@ -0,0 +1,165 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cng-bo-v-pht-thanh-truyn-hnh-tuyn-quang",
+      "name": "Cổng Báo và Phát Thanh, Truyền hình Tuyên Quang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.22431448119055375,
+        "pitch": -0.3345266511709326,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-phng-k-thut-trng-quay",
+      "name": "Phòng Kỹ thuật trường quay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.9272150729555371,
+        "pitch": -0.007622375536444181,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.422746801535297,
+          "pitch": 0.06234512555519167,
+          "rotation": 0,
+          "target": "2-trng-quay-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-trng-quay-03",
+      "name": "Trường quay 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.31207874042284445,
+        "pitch": 0.05930050175578394,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.266999393789723,
+          "pitch": 0.0009910527861496377,
+          "rotation": 0,
+          "target": "1-phng-k-thut-trng-quay"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-phng-k-thut-sn-xut-chng-trnh-truyn-hnh",
+      "name": "Phòng Kỹ thuật sản xuất Chương trình truyền hình",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.16559380221985265,
+        "pitch": 0.13947288568201088,
+        "fov": 1.489925106613882
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
